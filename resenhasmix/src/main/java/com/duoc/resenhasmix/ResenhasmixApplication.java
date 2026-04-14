59355e63109163bcad499872e217ef5e0e953dec
javiehh <javie.herrerag@duocuc.cl>
2026-04-14 14:06:33 -0400

    Subida del proyecto
    primera subida

---
+++ resenhasmix/src/main/java/com/duoc/resenhasmix/ResenhasmixApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.resenhasmix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResenhasmixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResenhasmixApplication.class, args);
+	}
+
+}
---
+++ resenhasmix/src/test/java/com/duoc/resenhasmix/ResenhasmixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.resenhasmix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResenhasmixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
